3a7ac69913b32b3b10961800ae872227a4cbd035
Chyld Medford <chyld.medford@gmail.com>
2016-02-29 02:11:57 +0000

    newlines

--- c9-setup.sh
+++ c9-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir ~/downloads
+
 cd ~/workspace
 rm README.md
 
@@ -17,5 +19,8 @@ sudo pip install httpie
 curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
 sudo mv cf /bin
 
-echo "edit ~/.gitconfig, then"
+printf "\n\n\n"
+echo "edit ~/.gitconfig"
+echo "delete the devops directory"
 echo "exit the terminal and restart"
+
