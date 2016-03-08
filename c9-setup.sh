0ddb41a02afd1a300c4bb76c4d71caedc8efde73
Chyld Medford <chyld.medford@gmail.com>
2016-03-08 15:25:23 -0800

    Update c9-setup.sh

--- c9-setup.sh
+++ c9-setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+GIT=2.7.2
+NODE=5.7
+
 sudo apt-get update
 sudo apt-get install -y apache2-utils libcurl4-openssl-dev
 
 mkdir ~/downloads && cd ~/downloads
-wget https://www.kernel.org/pub/software/scm/git/git-2.7.2.tar.xz
-tar -xvf git-2.7.2.tar.xz
-cd ~/downloads/git-2.7.2
+wget https://www.kernel.org/pub/software/scm/git/git-${GIT}.tar.xz
+tar -xvf git-${GIT}.tar.xz
+cd ~/downloads/git-${GIT}
 make prefix=/home/ubuntu/.local/git all
 make prefix=/home/ubuntu/.local/git install
 cd ~/downloads && rm -rf git*
@@ -19,9 +22,9 @@ wget https://raw.githubusercontent.com/chyld/devops/master/dotfiles/c9/.bash_pro
 source ~/.profile
 source ~/.bash_profile
 
-nvm install 5.7
-nvm alias default 5.7
-nvm use 5.7
+nvm install ${NODE}
+nvm alias default ${NODE}
+nvm use ${NODE}
 npm install -g nodemon mocha gulp
 
 sudo pip install httpie
