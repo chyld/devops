0c25d53baccc9d0f50cf98f3a5749b4dd8e5d878
Chyld Medford <chyld.medford@gmail.com>
2016-03-08 15:21:57 -0800

    Update c9-setup.sh

--- c9-setup.sh
+++ c9-setup.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y apache2-utils
+sudo apt-get install -y apache2-utils libcurl4-openssl-dev
 
-mkdir ~/downloads
+mkdir ~/downloads && cd ~/downloads
+wget https://www.kernel.org/pub/software/scm/git/git-2.7.2.tar.xz
+tar -xvf git-2.7.2.tar.xz
+cd ~/downloads/git-2.7.2
+make prefix=/home/ubuntu/.local/git all
+make prefix=/home/ubuntu/.local/git install
+cd ~/downloads && rm -rf git*
 
 cd ~
 rm .bash_aliases .gitconfig .gitignore
-wget https://raw.githubusercontent.com/chyld/devops/master/dotfiles/ubuntu-ec2/.bash_aliases
-wget https://raw.githubusercontent.com/chyld/devops/master/dotfiles/ubuntu-ec2/.gitconfig
+wget https://raw.githubusercontent.com/chyld/devops/master/dotfiles/c9/.bash_aliases
+wget https://raw.githubusercontent.com/chyld/devops/master/dotfiles/c9/.gitconfig
+wget https://raw.githubusercontent.com/chyld/devops/master/dotfiles/c9/.bash_profile
 source ~/.profile
+source ~/.bash_profile
 
 nvm install 5.7
 nvm alias default 5.7
