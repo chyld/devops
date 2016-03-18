205d5c81e005abe4f5a219b2cbb469b8642cf419
Chyld Medford <chyld.medford@gmail.com>
2016-03-18 11:15:50 -0700

    Update c9-setup.sh

--- c9-setup.sh
+++ c9-setup.sh
@@ -48,6 +48,7 @@ cd ~/downloads
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/.local/conda
 rm Miniconda3-latest-Linux-x86_64.sh
+source ~/.bash_profile
 
 # HTTPIE
 cd ~
