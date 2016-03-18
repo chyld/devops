22a28b07277e01e63cfee2284a7403bd742891de
Chyld Medford <chyld.medford@gmail.com>
2016-03-18 10:44:38 -0700

    Update c9-setup.sh

--- c9-setup.sh
+++ c9-setup.sh
@@ -46,7 +46,7 @@ npm install -g nodemon mocha gulp localtunnel
 # PYTHON
 cd ~/downloads
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/.local/miniconda
+bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/.local/conda
 rm Miniconda3-latest-Linux-x86_64.sh
 
 # HTTPIE
@@ -58,6 +58,7 @@ cd ~/downloads
 curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
 sudo mv cf /bin
 
+### DONE
 echo "*************************************************************************"
 echo "*************************************************************************"
 echo "*************************************************************************"
