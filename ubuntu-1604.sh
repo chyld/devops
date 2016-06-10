fa2bde608c695bb4d344c691429c3ae638d52dde
Chyld Medford <chyld.medford@gmail.com>
2016-06-10 11:38:12 -0700

    Update ubuntu-1604.sh

--- ubuntu-1604.sh
+++ ubuntu-1604.sh
@@ -43,7 +43,20 @@ cd ~/Downloads
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/.local/conda
 pip install httpie ipdb http-prompt
-conda install -y jupyter numpy scipy sympy pandas statsmodels numexpr scikit-learn unicodecsv csvkit seaborn bokeh matplotlib
+# conda install -y jupyter numpy scipy sympy pandas statsmodels numexpr scikit-learn unicodecsv csvkit seaborn bokeh matplotlib
+
+# DOCKER
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo sh -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' >> /etc/apt/sources.list.d/docker.list"
+sudo apt-get update
+sudo apt-get purge lxc-docker
+apt-cache policy docker-engine
+sudo apt-get install linux-image-extra-$(uname -r)
+sudo apt-get install docker-engine -y
+sudo service docker start
+sudo groupadd docker
+sudo usermod -aG docker chyld
 
 # CONFIGURATION
 cd ~/Downloads
