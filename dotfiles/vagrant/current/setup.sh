01666861ea4a0e86e8102cdf79f54e6ce4e2a483
Chyld Medford <chyld.medford@gmail.com>
2017-09-21 21:08:51 -0700

    sync

--- dotfiles/vagrant/current/setup.sh
+++ dotfiles/vagrant/current/setup.sh
@@ -42,4 +42,5 @@ ln -s ~/Code/devops/dotfiles/vagrant/current/.bash_profile .
 ln -s ~/Code/devops/dotfiles/vagrant/current/.bash_colors .
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/colors/molokai.vim --create-dirs https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
 vim +PlugInstall +qall
