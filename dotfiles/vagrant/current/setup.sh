5aac0f0b0b18f14d55f6dc7bbee01e41a7a60728
Chyld Medford <chyld.medford@gmail.com>
2017-09-22 18:11:33 -0700

    sync

--- dotfiles/vagrant/current/setup.sh
+++ dotfiles/vagrant/current/setup.sh
@@ -5,6 +5,7 @@ NODE_URL=https://nodejs.org/dist/v8.5.0/node-v8.5.0-linux-x64.tar.xz
 CONDA_URL=https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 LOCAL=$HOME/.local
 
+# system
 sudo add-apt-repository -y ppa:jonathonf/vim
 sudo add-apt-repository ppa:git-core/ppa -y
 sudo apt-get update
@@ -20,27 +21,35 @@ sudo usermod -a -G docker $USER
 rm docker.deb
 # ------------------------------------------------------------------------------------------------------- #
 
+# directories
 cd ~
 mkdir $LOCAL
 mkdir -p $HOME/{Code,Data,Temp}
 mkdir -p $HOME/Code/go/{bin,pkg,src}
 
-# git install // system
+# go, node, python
 curl ${GO_URL} | tar xz && mv go $LOCAL/go
 curl ${NODE_URL} | tar xJ && mv node-v8.5.0-linux-x64 $LOCAL/node
 wget ${CONDA_URL} -O conda.sh && bash conda.sh -b -p $LOCAL/miniconda3
 rm conda.sh
 
+# code
 cd $HOME/Code && git clone https://github.com/chyld/devops
 cd ~
 
-ln -s ~/Code/devops/dotfiles/vagrant/current/.tmux.conf .
+# tmux
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+
+# dotfiles
 ln -s ~/Code/devops/dotfiles/vagrant/current/.vimrc .
 ln -s ~/Code/devops/dotfiles/vagrant/current/.gitconfig .
 ln -s ~/Code/devops/dotfiles/vagrant/current/.bash_aliases .
 ln -s ~/Code/devops/dotfiles/vagrant/current/.bash_profile .
 ln -s ~/Code/devops/dotfiles/vagrant/current/.bash_colors .
 
+# setup vim
 source $HOME/.bash_profile
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.vim/colors/molokai.vim --create-dirs https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
@@ -50,5 +59,6 @@ do
   vim +PlugInstall +qall
 done
 
-conda install -y scipy numpy jupyter sympy pandas
+# data science
+conda install -y scipy numpy jupyter sympy pandas matplotlib seaborn bokeh statsmodels
 pip install httpie sklearn
