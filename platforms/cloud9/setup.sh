9a58c2851aee8ee186132087b5ea4a936a4fc57a
Chyld Medford <chyld.medford@gmail.com>
2018-10-24 14:41:45 -0700

    Update setup.sh

--- platforms/cloud9/setup.sh
+++ platforms/cloud9/setup.sh
@@ -16,13 +16,21 @@ nvm use ${NODE}
 cd ~/downloads
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/.local/conda
-rm Miniconda3-latest-Linux-x86_64.sh
 
 # JAVA
 sudo yum -y update
 sudo yum -y install java-1.8.0-openjdk-devel
 sudo update-alternatives --set java /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java
 sudo update-alternatives --set javac /usr/lib/jvm/java-1.8.0-openjdk.x86_64/bin/javac
+
+cd ~/downloads
+wget http://mirror.metrocast.net/apache/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz
+tar -xvf apache-maven-3.5.4-bin.tar.gz
+mv apache-maven-3.5.4 ~/.local/maven
+
+# CLEAN UP
+cd ~/downloads
+rm -rf *
 
 ### DONE
 echo "*************************************************************************"
