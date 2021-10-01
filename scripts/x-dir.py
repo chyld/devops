216c1acc19b266485bc577ed51836837517fb855
Chyld Medford <chyld.medford@gmail.com>
2021-10-01 11:22:04 -0700

    fix xd

--- scripts/x-dir.py
+++ scripts/x-dir.py
@@ -3,6 +3,7 @@
 import os
 import uuid
 import pandas as pd
+from datetime import datetime
 
 # change the directory to *this* directory
 # so i can read the elements.csv file
@@ -15,10 +16,12 @@ x_dir = '/home/chyld/X'
 # generate name of folder
 df = pd.read_csv('elements.csv')
 element = df.Element.sample().values[0]
-symbol=df[df.Element == element].Symbol.values[0]
+symbol=df[df.Element == element].Symbol.values[0].lower().strip()
 atomic_number=df[df.Element == element].AtomicNumber.values[0]
-hexid = uuid.uuid4().hex[-10:]
-project_dir = f"{element.lower().strip()}-{symbol.lower().strip()}-{atomic_number}-{hexid}.x"
+element = element.lower().strip()
+hexid = uuid.uuid4().hex[-3:]
+current = datetime.now().strftime("%Y%m%d-%H%M%S")
+project_dir = f"{element}-{current}-{hexid}.x"
 
 # construct the destination directory
 dst = f'{x_dir}/{project_dir}'
