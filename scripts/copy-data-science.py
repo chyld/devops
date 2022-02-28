509a3370c48c8662226a4f30784de30583be0211
Chyld Medford <chyld.medford@gmail.com>
2022-02-27 17:37:35 -0800

    update

--- scripts/copy-data-science.py
+++ scripts/copy-data-science.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
 import os
+import uuid
 
 cwd = os.getcwd()
 source = "/home/chyld/Code/devops/templates/data-sci"
-command = f"cp -r {source}/* {cwd}"
+
+hexid = uuid.uuid4().hex[-4:]
+command = f"mkdir -p {cwd}/{hexid}.ds"
+os.system(command)
+
+command = f"cp -r {source}/* {cwd}/{hexid}.ds"
 os.system(command)
