b15e8d22aa1712e9c9d947eee584446dadc8fce2
Chyld Medford <chyld.medford@gmail.com>
2022-12-16 15:15:26 -0800

    neo vim

---
+++ platforms/mint-ubuntu/v012/nvim/after/plugin/lsp.lua
@@ -0,0 +1,5 @@
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+lsp.setup()
+
--- platforms/mint-ubuntu/v012/nvim/lua/chyld/general.lua
+++ platforms/mint-ubuntu/v012/nvim/lua/chyld/general.lua
@@ -1,3 +1,27 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 5
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "100"
+
 --vim.cmd("colorscheme nightfox")
 --require('lualine').setup {
 --   options = { theme = 'molokai' }
@@ -12,4 +36,134 @@
 --require'lspconfig'.pyright.setup {}
 
 --require('neoscroll').setup()
+
+--vim.cmd("colorscheme nightfox")
+--require('lualine').setup {
+--   options = { theme = 'molokai' }
+--}
+--require('tabby').setup()
+--require('dressing').setup()
+--require('legendary').setup()
+--require("mason").setup()
+--require("mason-lspconfig").setup()
+
+--require("lspconfig").sumneko_lua.setup {}
+--require'lspconfig'.pyright.setup {}
+
+--vim.cmd("colorscheme nightfox")
+--require('lualine').setup {
+--   options = { theme = 'molokai' }
+--}
+--require('tabby').setup()
+--require('dressing').setup()
+--require('legendary').setup()
+--require("mason").setup()
+--require("mason-lspconfig").setup()
+
+--require("lspconfig").sumneko_lua.setup {}
+--require'lspconfig'.pyright.setup {}
+
+--vim.cmd("colorscheme nightfox")
+--require('lualine').setup {
+--   options = { theme = 'molokai' }
+--}
+--require('tabby').setup()
+--require('dressing').setup()
+--require('legendary').setup()
+--require("mason").setup()
+--require("mason-lspconfig").setup()
+
+--require("lspconfig").sumneko_lua.setup {}
+--require'lspconfig'.pyright.setup {}
+
+--vim.cmd("colorscheme nightfox")
+--require('lualine').setup {
+--   options = { theme = 'molokai' }
+--}
+--require('tabby').setup()
+--require('dressing').setup()
+--require('legendary').setup()
+--require("mason").setup()
+--require("mason-lspconfig").setup()
+
+--require("lspconfig").sumneko_lua.setup {}
+--require'lspconfig'.pyright.setup {}
+
+--vim.cmd("colorscheme nightfox")
+--require('lualine').setup {
+--   options = { theme = 'molokai' }
+--}
+--require('tabby').setup()
+--require('dressing').setup()
+--require('legendary').setup()
+--require("mason").setup()
+--require("mason-lspconfig").setup()
+
+--require("lspconfig").sumneko_lua.setup {}
+--require'lspconfig'.pyright.setup {}
+
+--vim.cmd("colorscheme nightfox")
+--require('lualine').setup {
+--   options = { theme = 'molokai' }
+--}
+--require('tabby').setup()
+--require('dressing').setup()
+--require('legendary').setup()
+--require("mason").setup()
+--require("mason-lspconfig").setup()
+
+--require("lspconfig").sumneko_lua.setup {}
+--require'lspconfig'.pyright.setup {}
+
+--vim.cmd("colorscheme nightfox")
+--require('lualine').setup {
+--   options = { theme = 'molokai' }
+--}
+--require('tabby').setup()
+--require('dressing').setup()
+--require('legendary').setup()
+--require("mason").setup()
+--require("mason-lspconfig").setup()
+
+--require("lspconfig").sumneko_lua.setup {}
+--require'lspconfig'.pyright.setup {}
+
+--vim.cmd("colorscheme nightfox")
+--require('lualine').setup {
+--   options = { theme = 'molokai' }
+--}
+--require('tabby').setup()
+--require('dressing').setup()
+--require('legendary').setup()
+--require("mason").setup()
+--require("mason-lspconfig").setup()
+
+--require("lspconfig").sumneko_lua.setup {}
+--require'lspconfig'.pyright.setup {}
+
+--vim.cmd("colorscheme nightfox")
+--require('lualine').setup {
+--   options = { theme = 'molokai' }
+--}
+--require('tabby').setup()
+--require('dressing').setup()
+--require('legendary').setup()
+--require("mason").setup()
+--require("mason-lspconfig").setup()
+
+--require("lspconfig").sumneko_lua.setup {}
+--require'lspconfig'.pyright.setup {}
+
+--vim.cmd("colorscheme nightfox")
+--require('lualine').setup {
+--   options = { theme = 'molokai' }
+--}
+--require('tabby').setup()
+--require('dressing').setup()
+--require('legendary').setup()
+--require("mason").setup()
+--require("mason-lspconfig").setup()
+
+--require("lspconfig").sumneko_lua.setup {}
+--require'lspconfig'.pyright.setup {}
 
