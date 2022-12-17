3840a2848d2523a702d5ac857353fb99dc014903
Chyld Medford <chyld.medford@gmail.com>
2022-12-17 02:59:27 -0800

    nvim

--- platforms/mint-ubuntu/v012/nvim/after/plugin/remap.lua
+++ platforms/mint-ubuntu/v012/nvim/after/plugin/remap.lua
@@ -12,3 +12,5 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
