fbc2524577915772bdb6923ae9956d865c8bcc16
Chyld Medford <chyld.medford@gmail.com>
2023-08-29 21:26:03 -0700

    auto sync

--- platforms/mint-ubuntu/v014/nvim/lua/chyld/init.lua
+++ platforms/mint-ubuntu/v014/nvim/lua/chyld/init.lua
@@ -22,6 +22,8 @@ vim.opt.hlsearch = false    -- does not show search terms highlighted, which is 
 vim.opt.wrap = false        -- do not wrap long lines
 vim.opt.scrolloff = 3       -- always show at least 3 spaces above and below cursor
 vim.opt.colorcolumn = '100' -- vertial stripe
+vim.opt.updatetime = 250    -- time for swap file to be written
+vim.opt.timeoutlen = 300    -- waits 300ms for a mapped sequence to complete
 
 -- ### ----------------------------------------------------------------------------------------- ###
 -- ### ----------------------------------------------------------------------------------------- ###
@@ -30,6 +32,7 @@ vim.opt.colorcolumn = '100' -- vertial stripe
 vim.g.mapleader = ' '       -- change leader key to spacebar
 vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')      -- in normal mode, save file
 vim.keymap.set('n', '<leader>q', '<cmd>quit<cr>')       -- in normal mode, quit
+vim.keymap.set('n', '<leader>t', '<cmd>Neotree<cr>')    -- in normal mode, Neotree
 
 -- ### ----------------------------------------------------------------------------------------- ###
 -- ### ----------------------------------------------------------------------------------------- ###
@@ -53,12 +56,33 @@ vim.opt.rtp:prepend(lazypath)
 -- ### ----------------------------------------------------------------------------------------- ###
 
 require("lazy").setup({
-  "folke/which-key.nvim",
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-  "folke/neodev.nvim",
-  'folke/tokyonight.nvim',
-  'nvim-lualine/lualine.nvim',
-  'nvim-tree/nvim-web-devicons',
+    "folke/which-key.nvim",
+    { "folke/neoconf.nvim", cmd = "Neoconf" },
+    "folke/neodev.nvim",
+    'folke/tokyonight.nvim',
+    'nvim-lualine/lualine.nvim',
+    'nvim-tree/nvim-web-devicons',
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        }
+    },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter-textobjects',
+        },
+        build = ':TSUpdate',
+    },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        -- or                              , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
 })
 
 -- ### ----------------------------------------------------------------------------------------- ###
@@ -68,6 +92,12 @@ require("lazy").setup({
 vim.opt.termguicolors = true
 vim.cmd.colorscheme('tokyonight')
 require('lualine').setup()
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 -- ### ----------------------------------------------------------------------------------------- ###
 -- ### ----------------------------------------------------------------------------------------- ###
