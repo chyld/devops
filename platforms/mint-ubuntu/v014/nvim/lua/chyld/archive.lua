ac2151a86ae9784db197ae50b9dd464656438174
Chyld Medford <chyld.medford@gmail.com>
2023-12-12 03:41:24 -0800

    auto sync

--- platforms/mint-ubuntu/v014/nvim/lua/chyld/archive.lua
+++ platforms/mint-ubuntu/v014/nvim/lua/chyld/archive.lua
@@ -56,15 +56,33 @@ vim.opt.rtp:prepend(lazypath)
 -- ### ----------------------------------------------------------------------------------------- ###
 
 require("lazy").setup({
-  "folke/which-key.nvim",
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-  "folke/neodev.nvim",
-  "folke/tokyonight.nvim",
-  'nvim-lualine/lualine.nvim',
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  }
+    "folke/which-key.nvim",
+    { "folke/neoconf.nvim", cmd = "Neoconf" },
+    "folke/neodev.nvim",
+    'folke/tokyonight.nvim',
+    'nvim-lualine/lualine.nvim',
+    'nvim-tree/nvim-web-devicons',
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        }
+    },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter-textobjects',
+        },
+        build = ':TSUpdate',
+    },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        -- or                              , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
 })
 
 -- ### ----------------------------------------------------------------------------------------- ###
