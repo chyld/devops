71fefa756865188ebed24602a05c1f76ff4e51a6
Chyld Medford <chyld.medford@gmail.com>
2024-03-03 02:34:39 -0800

    auto sync

--- templates/bash/script.sh
+++ templates/bash/script.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-set -x                  # debugging
-set -euo pipefail       # check any errors including unset variables and errors in pipelines
-trap exitf EXIT
+set -x                              # debugging
+set -euo pipefail                   # check any errors including unset variables and errors in pipelines
+trap exitf EXIT                     # cleanup before script exit
 
 func() {
-    trap returnf RETURN
+    trap returnf RETURN             # cleanup before function returns
+    local var                       # declare variables separately
+    var=$(ls > /dev/null; pwd)      # use subprocesses
+    echo "var: $var"                # always quote variables
 }
 
 returnf() {
