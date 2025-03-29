20c722ce3821d332e543c0eaf2d53acaf337a902
Chyld Medford <chyld.medford@gmail.com>
2025-03-28 19:25:40 -0700

    path.py

--- dots/scripts/path.py
+++ dots/scripts/path.py
@@ -15,9 +15,9 @@ from rich.console import Console
 console = Console()
 paths = os.environ['PATH'].split(os.pathsep)
 
-for i, path in enumerate(paths):
+for i, path in enumerate([p for p in paths if "cache" not in p]):
     if i % 2 == 0:
-        console.print(f"[bold red]{i}[/bold red] [yellow]{path}[/yellow]")
+        console.print(f"[bold red]{i}[/bold red] : [yellow]{path}[/yellow]")
     else:
-        console.print(f"[bold red]{i}[/bold red] [blue]{path}[/blue]")
+        console.print(f"[bold red]{i}[/bold red] : [blue]{path}[/blue]")
 
