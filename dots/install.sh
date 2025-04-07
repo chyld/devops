aa204d9af298d79e8aadce5fe0e121065efbedd6
Chyld Medford <chyld.medford@gmail.com>
2025-04-06 23:52:25 -0700

    mpv input.conf

--- dots/install.sh
+++ dots/install.sh
@@ -2,8 +2,8 @@
 
 # Check if an argument was provided
 if [ $# -eq 0 ]; then
-    echo "No arguments provided. Usage: ./install.sh <mac or linux>"
-    exit 1
+  echo "No arguments provided. Usage: ./install.sh <mac or linux>"
+  exit 1
 fi
 
 # Store the first argument in a variable
@@ -14,29 +14,28 @@ mkdir -p $HOME/.bin
 mkdir -p $HOME/.config/{ghostty,uv}
 
 # System agnostic files
-ln -s $HOME/Developer/devops/dots/.gitconfig             $HOME
-ln -s $HOME/Developer/devops/dots/.tmux.conf             $HOME
-ln -s $HOME/Developer/devops/dots/.bin/*                 $HOME/.bin
-ln -s $HOME/Developer/devops/dots/.config/starship.toml  $HOME/.config
+ln -s $HOME/Developer/devops/dots/.gitconfig $HOME
+ln -s $HOME/Developer/devops/dots/.tmux.conf $HOME
+ln -s $HOME/Developer/devops/dots/.bin/* $HOME/.bin
+ln -s $HOME/Developer/devops/dots/.config/starship.toml $HOME/.config
 ln -s $HOME/Developer/devops/dots/.config/ghostty/config $HOME/.config/ghostty
-ln -s $HOME/Developer/devops/dots/.config/uv/uv.toml     $HOME/.config/uv
-ln -s $HOME/Developer/devops/dots/.config/mpv/mpv.conf   $HOME/.config/mpv
+ln -s $HOME/Developer/devops/dots/.config/uv/uv.toml $HOME/.config/uv
+ln -s $HOME/Developer/devops/dots/.config/mpv/* $HOME/.config/mpv
 
 # System agnostic directories
-cp -r $HOME/Developer/devops/dots/.dockerfiles           $HOME
+cp -r $HOME/Developer/devops/dots/.dockerfiles $HOME
 
-# System specific 
+# System specific
 case "$system" in
-    "mac")
-        echo "linking MAC specific files"
-        ln -s $HOME/Developer/devops/dots/.zshrc__MAC    $HOME/.zshrc
-        ;;
-    "linux")
-        echo "linking LINUX specific files"
-        ln -s $HOME/Developer/devops/dots/.bashrc__LINUX $HOME/.bashrc
-        ;;
-    *)
-        echo "Unknown argument: $system"
-        ;;
+"mac")
+  echo "linking MAC specific files"
+  ln -s $HOME/Developer/devops/dots/.zshrc__MAC $HOME/.zshrc
+  ;;
+"linux")
+  echo "linking LINUX specific files"
+  ln -s $HOME/Developer/devops/dots/.bashrc__LINUX $HOME/.bashrc
+  ;;
+*)
+  echo "Unknown argument: $system"
+  ;;
 esac
-
