e28f272a12ad3cc55eb13106d0656087b97bbd4a
Chyld Medford <chyld.medford@gmail.com>
2026-04-04 14:58:36 -0700

    cachyos

---
+++ dots/cachy/.config/fish/config.fish
@@ -0,0 +1,62 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+function vi
+    nvim $argv
+end
+
+function cat
+    bat $argv
+end
+
+function ef
+    nvim $HOME/.config/fish/config.fish
+end
+
+function c
+    clear
+end
+
+function b
+    cd ..
+end
+
+function l
+    eza -al      --icons --group-directories-first $argv
+end
+
+function ll
+    eza -aT -L 2 --icons --group-directories-first $argv
+end
+
+function lll
+    eza -aT      --icons --group-directories-first $argv
+end
+
+function cl
+    c
+    l
+end
+
+function cll
+    c
+    ll
+end
+
+function clll
+    c
+    lll
+end
+
+function dl
+    cd ~/Downloads
+end
+
+function v
+    set timestamp (date +%y%m%d-%H%M)
+    set suffix (printf '%03x' (math (random) % 4096))
+    set dirname $HOME/Void/$timestamp.$suffix
+
+    mkdir -p $dirname
+    and cd $dirname
+end
+
